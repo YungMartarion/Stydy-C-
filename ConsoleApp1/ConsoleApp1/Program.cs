7853134afe562916ecf402de570da55a12b33379
YungMartarion <80800166+YungMartarion@users.noreply.github.com>
2021-04-05 20:24:52 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,11 +9,14 @@ namespace ConsoleApp1
             int a = 3;
             int b = 5;
             int d = a + b;
+            
+            double bez = (a++*a++)/b;
 
             double z = --b + b++;
 
             Console.WriteLine(d);
             Console.WriteLine(z);
+            Console.WriteLine(bez);
             Console.ReadKey();
 
         }
